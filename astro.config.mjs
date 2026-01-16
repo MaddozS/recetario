260ce7ae056e5a18661a64cd3b3f53e5944bfc5c
Axel Anaya <axel.ana25@gmail.com>
2026-01-16 07:58:56 -0600

    Configure Astro with Tailwind CSS and icon integration, and set up redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import icon from 'astro-icon';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: ([tailwindcss()])
+  },
+
+  integrations: [icon({
+    include: {
+      lucide: ['*'], // Loads entire Lucide icon set
+    },
+  })],
+
+  redirects: {
+    "/": "/recipes/101",
+    "/recipes": "/recipes/101"
+  }
+});
