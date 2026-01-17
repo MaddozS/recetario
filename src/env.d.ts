26943303b0dca8fc9675c9ccc50888a2a6b99845
Axel Anaya <axel.ana25@gmail.com>
2026-01-17 00:16:17 -0600

    Add TypeScript definitions for better-auth integration and environment variables

---
+++ src/env.d.ts
@@ -0,0 +1,21 @@
+
+/// <reference path="../.astro/types.d.ts" />
+declare namespace App {
+  // Note: 'import {} from ""' syntax does not work in .d.ts files.
+  interface Locals {
+      user: import("better-auth").User | null;
+      session: import("better-auth").Session | null;
+  }
+  interface ImportMetaEnv {
+    readonly BETTER_AUTH_SECRET: string;
+    readonly BETTER_AUTH_URL: string;
+
+    readonly GOOGLE_CLIENT_ID: string;
+    readonly GOOGLE_CLIENT_SECRET: string;
+  }
+
+  interface ImportMeta {
+    readonly env: ImportMetaEnv;
+  }
+}
+
